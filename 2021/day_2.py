bd6ff67c12b612b0119820ef7a103ef608d107eb
Žiga Opara <ziga.opara@bitstamp.net>
2021-12-02 12:36:19 +0100

    Complete day 2

---
+++ 2021/day_2.py
@@ -0,0 +1,42 @@
+import helpers
+
+def part_one(input):
+    horizontal = 0
+    vertical = 0
+    for command in input:
+        if command.startswith('forward'):
+            amount = int(command[8:])
+            horizontal += amount
+        if command.startswith('down'):
+            amount = int(command[5:])
+            vertical += amount
+        if command.startswith('up'):
+            amount = int(command[3:])
+            vertical -= amount
+    result = horizontal * vertical
+    return result
+
+
+def part_two(input):
+    horizontal = 0
+    vertical = 0
+    aim = 0
+    for command in input:
+        if command.startswith('forward'):
+            amount = int(command[8:])
+            horizontal += amount
+            vertical += amount * aim
+        if command.startswith('down'):
+            amount = int(command[5:])
+            aim += amount
+        if command.startswith('up'):
+            amount = int(command[3:])
+            aim -= amount
+    result = horizontal * vertical
+    return result
+
+input = helpers.load_puzzle_input(2)
+
+
+print('Part one result: %s'%(helpers.run_and_time(part_one, input)))
+print('Part two result: %s'%(helpers.run_and_time(part_two, input)))
